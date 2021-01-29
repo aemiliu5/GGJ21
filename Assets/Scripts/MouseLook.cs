037aa13e93366630f2d902fafe86b1bef70a9f83
Dimitris-Provatas <james.provatas@gmail.com>
2021-01-29 17:20:21 +0200

    camera mouse look fix

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -6,15 +6,13 @@ public class MouseLook : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
     public float clampTop, clampBottom;
-    
-    private Transform playerBody;
-    private float xRotation = 0f;
+
+    private float xRotation = 0f, yRotation = 0f;
 
     void Awake()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        playerBody = transform.parent.transform;
     }
 
     void Update()
@@ -22,7 +20,8 @@ public class MouseLook : MonoBehaviour
         xRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
         xRotation = Mathf.Clamp(xRotation, -clampTop, clampBottom);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime);
+        yRotation -= Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
     }
 }
