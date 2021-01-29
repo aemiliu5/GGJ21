fd055f0c41e00e55a468721685c1056fa89c7f9e
Dimitris-Provatas <james.provatas@gmail.com>
2021-01-29 16:54:04 +0200

    camera mouse look

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public float clampTop, clampBottom;
+    
+    private Transform playerBody;
+    private float xRotation = 0f;
+
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        playerBody = transform.parent.transform;
+    }
+
+    void Update()
+    {
+        xRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        xRotation = Mathf.Clamp(xRotation, -clampTop, clampBottom);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime);
+    }
+}
