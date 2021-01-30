b779bf2ce965d6ccc0493efea910a23bfebb1cca
Aimilios Seimenis <seimenace@gmail.com>
2021-01-30 19:28:09 +0200

    Various prefab work, fixed shadows, conveyor belt script

---
+++ Assets/ConveyorBelt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public float speed;
+    public Vector3 direction;
+    public List<GameObject> onBelt;
+    
+    private void FixedUpdate()
+    {
+        for (int i = 0; i <= onBelt.Count - 1; i++)
+        {
+            onBelt[i].GetComponent<Rigidbody>().velocity = speed * direction * Time.fixedDeltaTime;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        onBelt.Add(other.gameObject);
+    }
+    
+    private void OnCollisionExit(Collision other)
+    {
+        onBelt.Remove(other.gameObject);
+    }
+}
