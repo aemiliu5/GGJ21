dc8ef0e9ca8b9f12b0599268a65ea4fca98097c2
Nickzouk <Nickzouk2000@gmail.com>
2021-01-30 05:04:38 +0200

    made menu :alarm_clock:  :point_right:  :point_left:  :disappointed_relieved:

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject SettingsMenu;
+    public GameObject CreditsMenu;
+    public Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
+    public Slider VolumeSlider;
+
+    public void Play()
+    {
+        
+    }
+
+   
+   
+
+    public void Settings()
+    {
+        MainMenu.SetActive(false);
+        SettingsMenu.SetActive(true);
+    }
+
+    public void Credits()
+    {
+        MainMenu.SetActive(false);
+        CreditsMenu.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        SettingsMenu.SetActive(false);
+        CreditsMenu.SetActive(false);
+        
+        MainMenu.SetActive(true);
+   
+    }
+
+
+    public void Awake()
+    {
+        GetAvailableResolutions();
+        AddUIListeners();
+    }
+
+    public void AddUIListeners()
+    {
+        resolutionDropdown.onValueChanged.AddListener(delegate { OnResolutionChange(); });
+        VolumeSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChange(); });
+    }
+
+    public void GetAvailableResolutions()
+    {
+        resolutions = Screen.resolutions;
+
+        foreach (Resolution resolution in resolutions)
+        {
+            resolutionDropdown.options.Add(new Dropdown.OptionData(resolution.ToString()));
+        }
+    }
+
+    public void OnResolutionChange()
+    {
+        Screen.SetResolution(resolutions[resolutionDropdown.value].width, resolutions[resolutionDropdown.value].height, Screen.fullScreen);
+        PlayerPrefs.SetInt("SettingsResolutionIndex", resolutionDropdown.value);
+    }
+
+    public void OnMusicVolumeChange()
+    {
+        AudioListener.volume = VolumeSlider.value; // affects menu music only
+        PlayerPrefs.SetFloat("SettingsVolumeMusic", VolumeSlider.value);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
