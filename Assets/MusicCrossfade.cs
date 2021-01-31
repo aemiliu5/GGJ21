01784ee72f14f2041c4ea00ba6c4abbb181c48d5
Aimilios Seimenis <seimenace@gmail.com>
2021-01-31 04:00:59 +0200

    Conveyor testing, prefab box colliders

---
+++ Assets/MusicCrossfade.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicCrossfade : MonoBehaviour
+{
+    public AudioSource filtered;
+    public AudioSource unfiltered;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(transform.eulerAngles);
+    }
+}
